4a300ef6aa176fe44aa4288fd8b766e99e2c2f8a
Mike Richards <mrichards42@gmx.com>
2015-04-18 23:00:31 -0500

    Redesign SingingActivity * Add tabs * Use loaders for queries * Use ListView instead of TextView for full minutes text (better performance when using very long strings)

--- app/src/main/java/org/fasola/fasolaminutes/SingingActivity.java
+++ app/src/main/java/org/fasola/fasolaminutes/SingingActivity.java
@@ -1,37 +1,31 @@
 package org.fasola.fasolaminutes;
 
+import android.database.Cursor;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 
 public class SingingActivity extends SimpleTabActivity {
-    public MinutesContract.SingingDAO mSinging;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        long id = getIntent().getLongExtra(MainActivity.EXTRA_ID, -1);
         super.onCreate(savedInstanceState);
         // Query
-        // TODO: query in a thread (all queries)
-        // This query really goes slowly when there is a long fullText (e.g. Camp)
-        mSinging = C.Singing.get(id);
-        if (mSinging != null) {
-            String name = mSinging.name.getString();
-            setTitle(name);
-            String location = mSinging.location.getString();
-            int nSongs = mSinging.songCount.getInt();
-            int nLeaders = mSinging.leaderCount.getInt();
-            String songs = getResources().getQuantityString(R.plurals.songsLed, nSongs, nSongs);
-            String leaders = getResources().getQuantityString(R.plurals.leaders, nLeaders, nLeaders);
-            ((TextView) findViewById(R.id.title)).setText(name);
-            ((TextView) findViewById(R.id.location)).setText(location);
-            ((TextView) findViewById(R.id.songs)).setText(songs);
-            ((TextView) findViewById(R.id.leaders)).setText(leaders);
-        }
+        long id = getIntent().getLongExtra(MainActivity.EXTRA_ID, -1);
+        SQL.Query query = C.Singing.select(C.Singing.name).whereEq(C.Singing.id);
+        getSupportLoaderManager().initLoader(1, null, new MinutesLoader(query, String.valueOf(id)) {
+            @Override
+            public void onLoadFinished(Cursor cursor) {
+                C.SingingDAO singing = C.Singing.fromCursor(cursor);
+                if (singing != null)
+                    setTitle(singing.name.getString());
+            }
+        });
     }
 
     @Override
@@ -46,19 +40,46 @@ public class SingingActivity extends SimpleTabActivity {
     }
 
     public static class SingingSongListFragment extends CursorListFragment {
-        public SingingSongListFragment() {
-            mItemLayoutId = android.R.layout.simple_list_item_2;
-            mIntentClass = SongActivity.class;
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View root = inflater.inflate(R.layout.fragment_singing_songlist, container, false);
+            inflateList(inflater, root, savedInstanceState);
+            return root;
         }
 
         @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            long singingId = getActivity().getIntent().getLongExtra(MainActivity.EXTRA_ID, -1);
-            SQL.Query query = C.Song.selectList(C.Song.fullName, C.Leader.fullName.func("group_concat", "', '"))
+        public void onViewCreated(final View view, Bundle savedInstanceState) {
+            setItemLayout(android.R.layout.simple_list_item_2);
+            setIntentActivity(SongActivity.class);
+            long id = getActivity().getIntent().getLongExtra(MainActivity.EXTRA_ID, -1);
+            // Singing info query
+            SQL.Query query = C.Singing.select(C.Singing.name, C.Singing.location,
+                                               C.Singing.songCount, C.Singing.leaderCount)
+                                        .whereEq(C.Singing.id);
+            getLoaderManager().initLoader(1, null, new MinutesLoader(query, String.valueOf(id)) {
+                @Override
+                public void onLoadFinished(Cursor cursor) {
+                    C.SingingDAO singing = C.Singing.fromCursor(cursor);
+                    if (singing != null) {
+                        String name = singing.name.getString();
+                        String location = singing.location.getString();
+                        int nSongs = singing.songCount.getInt();
+                        int nLeaders = singing.leaderCount.getInt();
+                        String songs = getResources().getQuantityString(R.plurals.songsLed, nSongs, nSongs);
+                        String leaders = getResources().getQuantityString(R.plurals.leaders, nLeaders, nLeaders);
+                        ((TextView) view.findViewById(R.id.title)).setText(name);
+                        ((TextView) view.findViewById(R.id.location)).setText(location);
+                        ((TextView) view.findViewById(R.id.songs)).setText(songs);
+                        ((TextView) view.findViewById(R.id.leaders)).setText(leaders);
+                    }
+                }
+            });
+            // Song list query
+            query = C.Song.selectList(C.Song.fullName, C.Leader.fullName.func("group_concat", "', '"))
                                 .whereEq(C.SongLeader.singingId)
                                 .order(C.SongLeader.singingOrder, "ASC");
-            setQuery(query, String.valueOf(singingId));
+            setQuery(query, String.valueOf(id));
+            super.onViewCreated(view, savedInstanceState);
         }
     }
 
@@ -70,11 +91,24 @@ public class SingingActivity extends SimpleTabActivity {
         }
 
         @Override
-        public void onViewCreated(View view, Bundle savedInstanceState) {
-            // Query
-            String fullText = ((SingingActivity) getActivity()).mSinging.fullText.getString();
-            ((TextView) view.findViewById(R.id.full_text)).setText(fullText.replace("\n", "\n\n"));
-            // Done
+        public void onViewCreated(final View view, Bundle savedInstanceState) {
+            long id = getActivity().getIntent().getLongExtra(MainActivity.EXTRA_ID, -1);
+            SQL.Query query = C.Singing.select(C.Singing.fullText).whereEq(C.Singing.id);
+            getLoaderManager().initLoader(1, null, new MinutesLoader(query, String.valueOf(id)) {
+                @Override
+                public void onLoadFinished(Cursor cursor) {
+                    C.SingingDAO singing = C.Singing.fromCursor(cursor);
+                    if (singing != null) {
+                        String text = singing.fullText.getString();
+                        // TextView chokes on very long text (e.g. full text for camp)
+                        // Fake it with a ListView of smaller TextViews for better performance
+                        ListView list = (ListView) view.findViewById(R.id.full_text);
+                        list.setAdapter(new ArrayAdapter<>(getActivity(),
+                                                           R.layout.long_text_item,
+                                                           text.split("\n")));
+                    }
+                }
+            });
             super.onViewCreated(view, savedInstanceState);
         }
     }
