92d6451cd9d266267003fc03ae3c2fb45aa80ab0
Mike Richards <mrichards42@gmx.com>
2015-09-19 18:07:01 -0500

    Fix db path in fuzzy_name

--- fuzzy_name.py
+++ fuzzy_name.py
@@ -8,7 +8,7 @@ from __future__ import print_function
 import sqlite3
 import re, string
 
-DATABASE_PATH = r'app/src/main/assets_/databases/minutes.db'
+DATABASE_PATH = r'app/src/main/assets/databases/minutes.db'
 
 # ('Description', 'pattern', 'replace")
 STEPS = (
