db1d382e19989c53faee39af147e97faff5a5c14
Mike Richards <mrichards42@gmx.com>
2015-09-19 19:11:53 -0500

    Update database

--- app/src/main/java/org/fasola/fasolaminutes/MinutesContract.java
+++ app/src/main/java/org/fasola/fasolaminutes/MinutesContract.java
@@ -10,7 +10,7 @@ public class MinutesContract {
     protected MinutesContract() {
     }
 
-    public static final int DB_VERSION = 4;
+    public static final int DB_VERSION = 5;
     public static final String DB_NAME = "minutes.db";
 
     // Contract classes (see below for definitions
