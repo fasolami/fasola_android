bd1642aa77816fa2e67e0061f3bb90540eb2d5ec
Mike Richards <mrichards42@gmx.com>
2016-01-08 13:10:38 -0500

    Increase error limit (effectively infinite retries)

--- app/src/main/java/org/fasola/fasolaminutes/PlaybackService.java
+++ app/src/main/java/org/fasola/fasolaminutes/PlaybackService.java
@@ -33,7 +33,7 @@ public class PlaybackService extends Service
             AudioManager.OnAudioFocusChangeListener {
 
     private static final String TAG = "PlaybackService";
-    private static final int ERROR_LIMIT = 10;
+    private static final int ERROR_LIMIT = 100;
     private static final int ERROR_DELAY_MS = 500;
 
     /** Enqueue songs */
