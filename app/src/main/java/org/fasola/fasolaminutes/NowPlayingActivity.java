210972d42183c585bc4e58366da0c7a5c4952b55
Mike Richards <mrichards42@gmx.com>
2016-02-09 22:11:21 -0500

    Add lead info to now playing

--- app/src/main/java/org/fasola/fasolaminutes/NowPlayingActivity.java
+++ app/src/main/java/org/fasola/fasolaminutes/NowPlayingActivity.java
@@ -1,17 +1,23 @@
 package org.fasola.fasolaminutes;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.MediaController;
+import android.widget.TextView;
 
 import java.util.List;
 
 
 public class NowPlayingActivity extends SimpleTabActivity {
-    long songId = -1;
+    Playlist.Song mSong;
     MediaController mController;
     PlaybackService.Control mPlayer;
 
@@ -20,6 +26,8 @@ public class NowPlayingActivity extends SimpleTabActivity {
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nowplaying);
+        // Save offscreen pages
+        mViewPager.setOffscreenPageLimit(mPagerAdapter.getCount());
         // Setup MediaController
         mController = (MediaController)findViewById(R.id.media_controller);
         mPlayer = new PlaybackService.Control(this);
@@ -58,18 +66,21 @@ public class NowPlayingActivity extends SimpleTabActivity {
             if (song != null) {
                 setTitle(song.name);
                 // Update fragments
-                if (song.songId != songId) {
-                    songId = song.songId;
+                if (song != mSong) {
+                    mSong = song;
                     List<Fragment> fragments = getSupportFragmentManager().getFragments();
                     if (fragments != null)
-                        for (Fragment fragment : fragments)
+                        for (Fragment fragment : fragments) {
                             if (fragment instanceof SongActivity.SongFragment)
-                                ((SongActivity.SongFragment) fragment).setSongId(songId);
+                                ((SongActivity.SongFragment) fragment).setSongId(mSong.songId);
+                            else if (fragment instanceof NowPlayingInfoFragment)
+                                ((NowPlayingInfoFragment) fragment).setLeadId(mSong.leadId);
+                        }
                 }
                 setProgressBarIndeterminateVisibility(service != null && service.isLoading());
             }
             else {
-                songId = -1;
+                mSong = null;
                 setProgressBarIndeterminateVisibility(false);
             }
         }
@@ -110,8 +121,93 @@ public class NowPlayingActivity extends SimpleTabActivity {
 
     @Override
     public void onNewFragment(Fragment fragment) {
+        if (mSong == null)
+            return;
         Bundle args = new Bundle();
-        args.putLong(CursorListFragment.EXTRA_ID, songId);
+        if (fragment instanceof SongActivity.SongFragment)
+            args.putLong(CursorListFragment.EXTRA_ID, mSong.songId);
+        else
+            args.putLong(CursorListFragment.EXTRA_ID, mSong.leadId);
         fragment.setArguments(args);
+    }
+
+    static public class NowPlayingInfoFragment extends CursorListFragment {
+        // For click handlers
+        long mSingingId = -1;
+        long mSongId = -1;
+        long mLeadId = -1;
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View root = inflater.inflate(R.layout.fragment_now_playing_info, container, false);
+            inflateList(inflater, (ViewGroup) root, savedInstanceState);
+            root.findViewById(R.id.song_name).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mSongId == -1)
+                        return;
+                    startActivity(new Intent(getActivity(), SongActivity.class)
+                            .putExtra(EXTRA_ID, mSongId));
+                }
+            });
+            root.findViewById(R.id.singing).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mSingingId == -1)
+                        return;
+                    startActivity(new Intent(getActivity(), SingingActivity.class)
+                            .putExtra(EXTRA_ID, mSingingId)
+                            .putExtra(SingingActivity.EXTRA_LEAD_ID, mLeadId));
+                }
+            });
+            return root;
+        }
+
+        @Override
+        public void onViewCreated(View view, Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            setItemLayout(android.R.layout.simple_list_item_1);
+            setIntentActivity(LeaderActivity.class);
+            setLeadId(getArguments().getLong(EXTRA_ID, -1));
+        }
+
+        public void setLeadId(long leadId) {
+            mLeadId = leadId;
+            getArguments().putLong(EXTRA_ID, leadId);
+            // List query
+            setQuery(SQL.select(C.Leader.id, C.Leader.fullName)
+                    .where(C.SongLeader.leadId, "=", leadId));
+            // Song/Singing query
+            SQL.Query query =
+                    SQL.select(C.Song.id, C.Song.fullName)
+                    .select(C.Singing.id, C.Singing.name, C.Singing.location, C.Singing.startDate)
+                    .from(C.SongLeader)
+                    .where(C.SongLeader.leadId, "=", leadId);
+            getLoaderManager().initLoader((int) leadId, null, new MinutesLoader(query) {
+                @Override
+                public void onLoadFinished(Cursor cursor) {
+                    if (!cursor.moveToFirst())
+                        return;
+                    View view = getView();
+                    mSongId = cursor.getLong(0);
+                    String songName = cursor.getString(1);
+                    mSingingId = cursor.getLong(2);
+                    String singingName = cursor.getString(3);
+                    String singingLocation = cursor.getString(4);
+                    String singingDate = cursor.getString(5);
+                    ((TextView) view.findViewById(R.id.song_name)).setText(songName);
+                    ((TextView) view.findViewById(R.id.singing_name)).setText(singingName);
+                    ((TextView) view.findViewById(R.id.singing_location)).setText(singingLocation);
+                    ((TextView) view.findViewById(R.id.singing_date)).setText(singingDate);
+                }
+            });
+        }
+
+        @Override
+        protected void setIntentData(Intent intent, int position, long id) {
+            super.setIntentData(intent, position, id);
+            // Set leadId for leader activity
+            intent.putExtra(SingingActivity.EXTRA_LEAD_ID, mLeadId);
+        }
     }
 }
