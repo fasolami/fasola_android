7700ce90a86505946d14267df8e8bd9f1927cce0
Mike Richards <mrichards42@gmx.com>
2018-01-12 18:34:41 -0500

    Update db for 2017

--- app/src/main/java/org/fasola/fasolaminutes/MinutesContract.java
+++ app/src/main/java/org/fasola/fasolaminutes/MinutesContract.java
@@ -15,9 +15,9 @@ public class MinutesContract {
     protected MinutesContract() {
     }
 
-    public static final int DB_VERSION = 12;
+    public static final int DB_VERSION = 13;
     public static final int MIN_YEAR = 1995;
-    public static final int MAX_YEAR = 2016;
+    public static final int MAX_YEAR = 2017;
     public static final String DB_NAME = "minutes.db";
 
     // Contract classes (see below for definitions
