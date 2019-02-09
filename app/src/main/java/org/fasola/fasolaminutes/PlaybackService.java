e4d62eefb8f41531e761d08b8106f3c8657a0693
Mike Richards <mrichards@democracy.works>
2019-02-08 21:43:33 -0500

    Fix notification on android >= 8.0
    Notifications have to have a channel now

--- app/src/main/java/org/fasola/fasolaminutes/PlaybackService.java
+++ app/src/main/java/org/fasola/fasolaminutes/PlaybackService.java
@@ -6,6 +6,7 @@
 package org.fasola.fasolaminutes;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
@@ -21,6 +22,8 @@ import android.os.Binder;
 import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.media.MediaMetadataCompat;
@@ -103,9 +106,10 @@ public class PlaybackService extends Service
     int mConnectionType = -1;
     Playlist.Song mSong; // Prepared song
     int mErrorCount; // Number of sequential errors
-    NotificationManager mNotificationManager;
+    NotificationManagerCompat mNotificationManager;
     Notification mNotification;
     private static final int NOTIFICATION_ID = 1;
+    private static final String CHANNEL_ID = "org.fasola.fasolaminutes.RECORDINGS_CHANNEL";
     AudioManager mAudioManager;
 
     MediaSessionCompat mMediaSession;
@@ -129,7 +133,8 @@ public class PlaybackService extends Service
         super.onCreate();
         mInstance = this;
         mControl = new Control(this);
-        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        createNotificationChannel();
+        mNotificationManager = NotificationManagerCompat.from(this);
         mConnectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         mAudioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
         mObserver.registerBroadcastReceiver(getApplicationContext());
@@ -489,18 +494,32 @@ public class PlaybackService extends Service
                 this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT
         );
         // Build Notification
-        Notification.Builder builder = new Notification.Builder(getApplicationContext())
-            .setSmallIcon(R.drawable.ic_stat_fasola)
-            .setContent(remote)
-            .setOngoing(true)
-            .setContentIntent(pendingIntent);
-        // Allow the notification to appear on the lockscreen, even if
-        // "hide sensitive notifications" is set
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-            builder.setVisibility(Notification.VISIBILITY_PUBLIC);
-        // build() was added in API 16
-        return builder.getNotification();
+        return new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_stat_fasola)
+                .setContent(remote)
+                .setOngoing(true)
+                .setContentIntent(pendingIntent)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .build();
     }
+
+    /** Create the NotificationChannel for api >= 26 */
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+
 
     boolean mHasMainTask = true;
     public void setMainTaskRunning(boolean isRunning) {
